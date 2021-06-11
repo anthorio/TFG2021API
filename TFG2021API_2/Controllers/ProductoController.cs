60231804a3ee34a0163a5c2ffff5cec2a945ee8a
Anthorio <anthorioyt@gmail.com>
2021-06-11 09:40:57 +0200

    Controlador de producto

---
+++ TFG2021API_2/Controllers/ProductoController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TFG2021API_2.Models.Response;
+using TFG2021API_2.Models;
+using TFG2021API_2.Models.Request;
+
+namespace TFG2021API_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Respuesta<List<Producto>> oRespuesta = new Respuesta<List<Producto>>();
+
+            try
+            {
+                using (TFG2021Context db = new TFG2021Context())
+                {
+                    var lst = db.Productos.ToList();
+                    oRespuesta.Exito = 1;
+                    oRespuesta.Data = lst;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpPost]
+        public IActionResult Add(ProductoRequest model)
+        {
+            Respuesta<List<Producto>> oRespuesta = new Respuesta<List<Producto>>();
+
+            try
+            {
+                using (TFG2021Context db = new TFG2021Context())
+                {
+                    Producto oProducto = new Producto();
+                    oProducto.ProductoId = model.ProductoID;
+                    oProducto.FamiliaProducto = model.Familia_Producto;
+                    oProducto.ProveedorProducto = model.Proveedor_Producto;
+                    oProducto.Nombre = model.Nombre;
+                    oProducto.Descripcion = model.Descripcion;
+                    oProducto.Cantidad = model.Cantidad;
+                    oProducto.Precio = model.Precio;
+                    oProducto.UrlImagen = model.URL_imagen;
+
+                    db.Productos.Add(oProducto);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpPut]
+        public IActionResult Edit(ProductoRequest model)
+        {
+            Respuesta<object> oRespuesta = new Respuesta<object>();
+
+            try
+            {
+                using (TFG2021Context db = new TFG2021Context())
+                {
+                    Producto oProducto = db.Productos.Find(model.ProductoID);
+                    oProducto.ProductoId = model.ProductoID;
+                    oProducto.FamiliaProducto = model.Familia_Producto;
+                    oProducto.ProveedorProducto = model.Proveedor_Producto;
+                    oProducto.Nombre = model.Nombre;
+                    oProducto.Descripcion = model.Descripcion;
+                    oProducto.Cantidad = model.Cantidad;
+                    oProducto.Precio = model.Precio;
+                    oProducto.UrlImagen = model.URL_imagen;
+
+                    db.Entry(oProducto).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int Id)
+        {
+            Respuesta<object> oRespuesta = new Respuesta<object>();
+
+            try
+            {
+                using (TFG2021Context db = new TFG2021Context())
+                {
+                    Producto oProducto = db.Productos.Find(Id);
+                    db.Remove(oProducto);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+    }
+}
---
+++ TFG2021API_2/Models/Request/ProductoRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TFG2021API_2.Models.Request
+{
+    public class ProductoRequest
+    {
+        public int ProductoID { get; set; }
+        public int Familia_Producto { get; set; }
+        public int Proveedor_Producto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public string URL_imagen { get; set; }
+    }
+}
